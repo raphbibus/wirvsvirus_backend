5fef063aa4c7cd9ad6bd233bc7060b602cf045ae
raphbibus <github@raphbib.us>
2020-03-21 16:40:14 +0100

    random number for md5 hash

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -36,7 +36,7 @@ class StatsController extends Controller
             $inOut = new InOut();
             $inOut->entered = $dt->toDateTimeString();
             $inOut->client_id = $client->id;
-            $inOut->token = md5($dt->toDateTimeString().$client->username);
+            $inOut->token = md5($dt->toDateTimeString().$client->username.rand(0,1000));
             $inOut->save();
 
             return response()->json($inOut, 201);
