2dba5b844244a2a83e9f7897b48d5cb629e3004a
Senik Hakobyan <sen.hakobyan@gmail.com>
2020-03-23 14:54:00 +0400

    User model dropped, Client model is now authenticatable

--- app/Client.php
+++ app/Client.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Client extends Model
+class Client extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
+    use Authenticatable, Authorizable;
 
     protected $table = 'clients';
 
@@ -18,8 +24,13 @@ class Client extends Model
         'username', 'display_name', 'points', 'seconds', 'nation', 'city',
     ];
 
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
     protected $hidden = [
-        'id',
+        'id', 'password',
     ];
 
     public function inouts()
@@ -27,4 +38,23 @@ class Client extends Model
         return $this->hasMany('App\InOut');
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
