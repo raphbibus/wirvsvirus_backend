b10e0da7702c21cc66c4912c8cd86f2b882f5564
Senik Hakobyan <sen.hakobyan@gmail.com>
2020-03-23 15:31:24 +0400

    login method implemented

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,18 @@ class AuthController extends ClientsController
 {
     public function login(Request $request)
     {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
 
+        $credentials = $request->only(['username', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid username or password.'], 401);
+        }
+
+        return $this->respondWithToken($token);
     }
 
     public function register(Request $request)
